72e170f94214b8c40f82ea582e5779618bbc8118
n1k01eta <ontypetzanne@gmail.com>
2018-06-11 16:49:58 +0300

    changes after review

--- app/src/main/java/com/example/android/inventoryapp/data/InventoryDbHelper.java
+++ app/src/main/java/com/example/android/inventoryapp/data/InventoryDbHelper.java
@@ -11,7 +11,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class InventoryDbHelper extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION = 1;
     private static final String DATABASE_NAME = "Store.db";
-
+    
     private static final String TEXT_TYPE = " TEXT";
     private static final String REAL_TYPE = " REAL";
     private static final String INTEGER_TYPE = " INTEGER";
